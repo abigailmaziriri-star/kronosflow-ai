f399b2854dd9df7772e620b73c69e32cca8d3612
abigailmaziriri-star <abigailmaziriri@gmail.com>
2026-09-18 01:26:03 +0200

    Change API token in app.py
    Updated API token for authentication.

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ for code, name in ALL_MARKETS.items():
     }
 
 APP_ID = "1089" 
-API_TOKEN = "pat_9303896ca66e1e132ebf393d548d4e858193eee9e9b783b700f317ac68a8e65a"
+API_TOKEN = "pat_2835a32815fff743180964079b2d7d66c61fbdb11dfabef674fadeb004f3f523"
 async def live_deriv_quantum_engine():
     global SYSTEM_TELEMETRY
     url = f"wss://://derivws.com{APP_ID}"
